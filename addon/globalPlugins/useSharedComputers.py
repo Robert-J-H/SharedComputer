5a7a9b0203864b05edd2cc2ede20506ed0c9df2e
Robert Hänggi <aarjay.robert@gmail.com>
2018-01-30 20:25:36 +0100

    Introduce volume correction
    IAudioEndpointVolume is now used to read and set mute state and levels.
    The sending of volume up/down is kept as fall-back procedure.
    Should for some reason the correction not work,so is it frequently
    amendable by reloading addons (Control+NVDA+F3).
    The GUI does currently not reflect the enhanced possibilities.

--- addon/globalPlugins/useSharedComputers.py
+++ addon/globalPlugins/useSharedComputers.py
@@ -2,21 +2,21 @@
 # useSharedComputers: Plugin to using shared computers
 # https://github.com/nvaccess/nvda/issues/4093
 # https://github.com/nvaccess/nvda/pull/7506/files
-# Copyright (C) 2018 Noelia Ruiz Martínez
+# Copyright (C) 2018 Noelia Ruiz Martínez, Robert Hänggi 
 # Released under GPL 2
 
 import globalPluginHandler
 import addonHandler
-import wx
-import gui
+import gui, wx, time, tones, winUser, config
 from gui import guiHelper
 from gui import nvdaControls
 from gui.settingsDialogs import SettingsDialog
-import config
-import winUser
 from keyboardHandler import KeyboardInputGesture
 from globalCommands import SCRCAT_CONFIG
-
+from comtypes import HRESULT,GUID,IUnknown, COMMETHOD, POINTER, CoCreateInstance, cast, c_float
+from ctypes.wintypes import BOOL, DWORD, UINT 
+from logHandler import log
+from api import processPendingEvents
 addonHandler.initTranslation()
 
 confspec = {
@@ -29,22 +29,44 @@ config.conf.spec["useSharedComputers"] = confspec
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def handleConfigProfileSwitch(self):
-		activation = config.conf["numLockManager"]["activation"]
+		activation = config.conf["useSharedComputers"]["activation"]
 		if activation < 2 and winUser.getKeyState(winUser.VK_NUMLOCK) != activation:
 			KeyboardInputGesture.fromName("numLock").send()
 
 	def changeVolumeLevel(self, volumeLevel):
-		from comtypes.client import CreateObject
-		wSObj = CreateObject("wScript.Shell")
-		# (174=volume down, 175=volume up, 173= mute)
-		wSObj.SendKeys('{'+chr(174) + ' ' + '100' + '}')
-		wSObj.SendKeys('{'+chr(175) + ' ' + str(volumeLevel) + '}')
+		speakers=getVolumeObject()
+		if speakers is not None:
+			# should actually work on first attempt
+			# but level 0 is a pain
+			# to do: level 0 and mute at the same time is for some reason not recognizable
+			for attempt in range(2):
+				processPendingEvents()
+				level=int(speakers.GetMasterVolumeLevelScalar()*100)
+				log.info("Level Speakers at Startup: {} Percent".format(level))
+				if level<volumeLevel:
+					speakers.SetMasterVolumeLevelScalar(volumeLevel/100.0,None)
+				muteState= speakers.GetMute()
+				log.info("Speakers at Startup: {}".format(("Unmuted","Muted")[muteState]))
+				if muteState:
+					speakers.SetMute(0, None)
+				time.sleep(0.05)
+				log.info("Speakers after correction: {} Percent, {}".format(
+					int(speakers.GetMasterVolumeLevelScalar()*100),
+					("Unmuted","Muted")[speakers.GetMute()]))
+		else:
+			# As a fall-back, change the volume "manually"
+			volDown=KeyboardInputGesture.fromName("VolumeDown")
+			volUp=KeyboardInputGesture.fromName("VolumeUp")
+			# one keystroke = 2 % here, is that universal?
+			repeats=volumeLevel//2
+			# We are only interested in the side effect, hence the dummy underscore variable
+			_ = {key.send() for key in iter((volDown,)*repeats + (volUp,)*repeats)}
+			time.sleep(float(volumeLevel)/62)
 
 	def __init__(self):
 		super(globalPluginHandler.GlobalPlugin, self).__init__()
 		volLevel = config.conf["useSharedComputers"]["volumeLevel"]
 		if config.conf["useSharedComputers"]["changeVolumeLevel"]:
-			# We use wx.CallAfter() so that NVDA can speak messages
 			wx.CallAfter(self.changeVolumeLevel, volLevel)
 		self.numLockState = winUser.getKeyState(winUser.VK_NUMLOCK)
 		self.handleConfigProfileSwitch()
@@ -112,3 +134,79 @@ class AddonSettingsDialog(SettingsDialog):
 		config.conf["useSharedComputers"]["activation"] = self.activateList.Selection
 		config.conf["useSharedComputers"]["changeVolumeLevel"] = self.changeVolumeLevelCheckBox.Value
 		config.conf["useSharedComputers"]["volumeLevel"] = self.volumeLevel.Value
+
+# Audio Stuff
+def getVolumeObject():
+	CLSID_MMDeviceEnumerator = GUID('{BCDE0395-E52F-467C-8E3D-C4579291692E}')
+	deviceEnumerator = CoCreateInstance(CLSID_MMDeviceEnumerator, IMMDeviceEnumerator, 1)
+	volObj = cast(
+		deviceEnumerator.GetDefaultAudioEndpoint(0, 1).Activate(IAudioEndpointVolume._iid_, 7, None),
+		POINTER(IAudioEndpointVolume))
+	return volObj
+
+# for a ffull-fletched Audio wrapper
+# visit https://github.com/AndreMiras/pycaw
+class IAudioEndpointVolume(IUnknown):
+	_iid_ = GUID('{5CDF2C82-841E-4546-9722-0CF74078229A}')
+	_methods_ = (
+		COMMETHOD([], HRESULT, 'NotImpl1'),
+		COMMETHOD([], HRESULT, 'NotImpl2'),
+		COMMETHOD([], HRESULT, 'GetChannelCount', (['out'], POINTER(UINT), 'pnChannelCount')),
+		COMMETHOD([], HRESULT, 'SetMasterVolumeLevel',
+			(['in'], c_float, 'fLevelDB'), (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'SetMasterVolumeLevelScalar',
+			(['in'], c_float, 'fLevel'), (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'GetMasterVolumeLevel', (['out'], POINTER(c_float), 'pfLevelDB')),
+		COMMETHOD([], HRESULT, 'GetMasterVolumeLevelScalar', (['out'], POINTER(c_float), 'pfLevelDB')),
+		COMMETHOD([], HRESULT, 'SetChannelVolumeLevel',
+			(['in'], UINT, 'nChannel'), (['in'], c_float, 'fLevelDB'), (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'SetChannelVolumeLevelScalar',
+			(['in'], DWORD, 'nChannel'), (['in'], c_float, 'fLevelDB'), (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'GetChannelVolumeLevel',
+			(['in'], UINT, 'nChannel'),
+			(['out'], POINTER(c_float), 'pfLevelDB')),
+		COMMETHOD([], HRESULT, 'GetChannelVolumeLevelScalar',
+			(['in'], DWORD, 'nChannel'),
+			(['out'], POINTER(c_float), 'pfLevelDB')),
+		COMMETHOD([], HRESULT, 'SetMute', (['in'], BOOL, 'bMute'), (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'GetMute', (['out'], POINTER(BOOL), 'pbMute')),
+		COMMETHOD([], HRESULT, 'GetVolumeStepInfo',
+			(['out'], POINTER(DWORD), 'pnStep'),
+			(['out'], POINTER(DWORD), 'pnStepCount')),
+		COMMETHOD([], HRESULT, 'VolumeStepUp', (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'VolumeStepDown', (['in'], POINTER(GUID), 'pguidEventContext')),
+		COMMETHOD([], HRESULT, 'QueryHardwareSupport', (['out'], POINTER(DWORD), 'pdwHardwareSupportMask')),
+		COMMETHOD([], HRESULT, 'GetVolumeRange',
+			(['out'], POINTER(c_float), 'pfMin'),
+			(['out'], POINTER(c_float), 'pfMax'),
+			(['out'], POINTER(c_float), 'pfIncr')))
+
+class IMMDevice(IUnknown):
+	_iid_ = GUID('{D666063F-1587-4E43-81F1-B948E807363F}')
+	_methods_ = (
+		COMMETHOD([], HRESULT, 'Activate',
+			(['in'], POINTER(GUID), 'iid'),
+			(['in'], DWORD, 'dwClsCtx'),
+			(['in'], POINTER(DWORD), 'pActivationParams'),
+			(['out'], POINTER(POINTER(IUnknown)), 'ppInterface')),)
+
+class IMMDeviceCollection(IUnknown):
+	_iid_ = GUID('{0BD7A1BE-7A1A-44DB-8397-CC5392387B5E}')
+	_methods_ = (
+		COMMETHOD([], HRESULT, 'GetCount',
+			(['out'], POINTER(UINT), 'pcDevices')),
+		COMMETHOD([], HRESULT, 'Item',
+			(['in'], UINT, 'nDevice'),
+			(['out'], POINTER(POINTER(IMMDevice)), 'ppDevice')))
+
+class IMMDeviceEnumerator(IUnknown):
+	_iid_ = GUID('{A95664D2-9614-4F35-A746-DE8DB63617E6}')
+	_methods_ = (
+		COMMETHOD([], HRESULT, 'EnumAudioEndpoints',
+			(['in'], DWORD, 'dataFlow'),
+			(['in'], DWORD, 'dwStateMask'),
+			(['out'], POINTER(POINTER(IMMDeviceCollection)), 'ppDevices')),
+		COMMETHOD([], HRESULT, 'GetDefaultAudioEndpoint',
+			(['in'], DWORD, 'dataFlow'),
+			(['in'], DWORD, 'role'),
+			(['out'], POINTER(POINTER(IMMDevice)), 'ppDevices')),)
